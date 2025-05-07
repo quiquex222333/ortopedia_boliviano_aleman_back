75c09c062c71df06eda402ac85511863fe9149f2
Enrique Rivera <juane222333@gmail.com>
2025-05-07 12:08:52 -0400

    Initial structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const userRoutes = require('./routes/user.routes');
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
